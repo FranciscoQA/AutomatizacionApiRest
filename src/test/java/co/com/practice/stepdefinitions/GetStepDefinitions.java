c1f3ceb86f8d94252d6ba500c99910730a42f520
FranciscoQA <147537557+FranciscoQA@users.noreply.github.com>
2024-03-01 02:13:31 -0500

    firt commit

---
+++ src/test/java/co/com/practice/stepdefinitions/GetStepDefinitions.java
@@ -0,0 +1,44 @@
+package co.com.practice.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GetStepDefinitions {
+
+    @Given("I Load customer information")
+    public void iLoadCustomerInformation(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+    @When("I call Get user API")
+    public void iCallGetUserAPI() {
+
+    }
+
+    @Then("I should see the status code {int}")
+    public void iShouldSeeTheStatusCode(int int1) {
+
+    }
+
+    @Then("I validate quantity key is {int}")
+    public void iValidateQuantityKeyIs(int int1) {
+
+    }
+
+    @Then("I validate schema response {string}")
+    public void iValidateSchemaResponse(String string) {
+
+    }
+
+    @Then("I validate fields get response api")
+    public void iValidateFieldsGetResponseApi() {
+
+    }
+
+    @Then("I validate get response contain data expected")
+    public void iValidateGetResponseContainDataExpected() {
+
+    }
+
+}
